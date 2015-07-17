fd16bad5f85e14260d5e7229f9259b295f138d38
Grant Tibbey <granttibbey@hotmail.com>
2015-07-17 20:23:16 +1000

    disable auto scaling alarms when updating desired capacity to stop scaling events when pooling

--- lib/aws_helpers/auto_scaling_group/update_desired_capacity.rb
+++ lib/aws_helpers/auto_scaling_group/update_desired_capacity.rb
@@ -5,7 +5,7 @@ module AwsHelpers
 
     class UpdateDesiredCapacity
 
-      def initialize(auto_scaling_client, elastic_load_balancing_client,  auto_scaling_group_name, desired_capacity, timeout)
+      def initialize(auto_scaling_client, elastic_load_balancing_client, auto_scaling_group_name, desired_capacity, timeout)
         @auto_scaling_client = auto_scaling_client
         @elastic_load_balancing_client = elastic_load_balancing_client
         @auto_scaling_group_name = auto_scaling_group_name
@@ -15,14 +15,24 @@ module AwsHelpers
 
       def execute
         puts "Setting #{@auto_scaling_group_name} desired capacity to #{@desired_capacity}"
-        @auto_scaling_client.set_desired_capacity(
-          :auto_scaling_group_name => @auto_scaling_group_name,
-          :desired_capacity => @desired_capacity)
-        auto_scaling_groups = @auto_scaling_client.describe_auto_scaling_groups(:auto_scaling_group_names => [@auto_scaling_group_name])[:auto_scaling_groups]
-        load_balancer_names = auto_scaling_groups.detect { |auto_scaling_group| auto_scaling_group[:auto_scaling_group_name] == @auto_scaling_group_name }[:load_balancer_names]
-        load_balancer_names.each { |load_balancer_name|
-          ElasticLoadBalancing::PollHealthyInstances.new(@elastic_load_balancing_client, load_balancer_name, @desired_capacity, @timeout).execute
-        }
+
+        begin
+          @auto_scaling_client.suspend_processes(
+            auto_scaling_group_name: @auto_scaling_group_name,
+            scaling_processes: ['AlarmNotification']
+          )
+          @auto_scaling_client.set_desired_capacity(
+            auto_scaling_group_name: @auto_scaling_group_name,
+            desired_capacity: @desired_capacity)
+          auto_scaling_groups = @auto_scaling_client.describe_auto_scaling_groups(auto_scaling_group_names: [@auto_scaling_group_name])[:auto_scaling_groups]
+          load_balancer_names = auto_scaling_groups.detect { |auto_scaling_group| auto_scaling_group[:auto_scaling_group_name] == @auto_scaling_group_name }[:load_balancer_names]
+          load_balancer_names.each { |load_balancer_name|
+            ElasticLoadBalancing::PollHealthyInstances.new(@elastic_load_balancing_client, load_balancer_name, @desired_capacity, @timeout).execute
+          }
+        ensure
+          @auto_scaling_client.resume_processes(auto_scaling_group_name: @auto_scaling_group_name)
+        end
+
 
       end
 
