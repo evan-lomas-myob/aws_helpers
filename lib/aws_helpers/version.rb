d022ebcfa1db18e7b2cf8c52ff7dfd410fb10737
Paul Borg <paul.borg@myob.com>
2015-07-23 15:10:15 +1000

    Revert "Add some comments to version for Yard to implement"
    This reverts commit f44649d4147d479aa0a1030fb0d6a1a4acce1947.

--- lib/aws_helpers/version.rb
+++ lib/aws_helpers/version.rb
@@ -1,9 +1,3 @@
-# Returns the current AWS Helpers VERSION
-#
-# @param none
-# @return [String] the current AwsHelpers version id
-
 module AwsHelpers
-  #Sets the VERSION id.
   VERSION = '2.0.1'
 end
