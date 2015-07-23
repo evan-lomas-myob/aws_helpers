84f530ca174d2daf6d52b398b1401db2965ce83b
Paul Borg <paul.borg@myob.com>
2015-07-23 11:58:08 +1000

    Add code climate configuration as travis-ci add on

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require "codeclimate-test-reporter"
 require 'aws_helpers'
+
+CodeClimate::TestReporter.start
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
