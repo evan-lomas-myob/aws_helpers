5d2bc5847f16c9c3a9fc613a096cde2386616ac3
Paul Borg <paul.borg@myob.com>
2015-07-23 16:07:55 +1000

    Add back requires in specs with a refactor for spec-helpers to work properly with CodeClimate. Code Climate working now

--- spec/aws_helpers_spec.rb
+++ spec/aws_helpers_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
+require 'aws_helpers'
 
 describe AwsHelpers do
   it 'has a version number' do
     expect(AwsHelpers::VERSION).not_to be nil
   end
+
+  it 'has a version number the equals 0.1.0' do
+    expect(AwsHelpers::VERSION).to eql('0.1.0')
+  end
+
 end
