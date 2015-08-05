fa3918f3089f5d6a92d1f49cac3af272045cb9a2
Paul Borg <paul.borg@myob.com>
2015-08-05 11:51:49 +1000

    Add class and spec for find images by tags and keeping original functionality

--- lib/aws_helpers/actions/ec2/images_find_by_tags.rb
+++ lib/aws_helpers/actions/ec2/images_find_by_tags.rb
@@ -11,6 +11,14 @@ module AwsHelpers
 
         def execute
 
+          client = @config.aws_ec2_client
+
+          filters = @tags.each do |tag|
+            {name: tag[:name], value: [tag[:value]]}
+          end
+
+          client.describe_images(filters)
+
         end
 
       end
