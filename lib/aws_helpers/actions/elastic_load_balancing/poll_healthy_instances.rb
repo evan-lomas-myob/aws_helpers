d0fe84212f3e61174953df468831d5bb4f0150d5
gstib <granttibbey@hotmail.com>
2015-08-05 16:32:54 +1000

    implement polling healthy instances

--- lib/aws_helpers/actions/elastic_load_balancing/poll_healthy_instances.rb
+++ lib/aws_helpers/actions/elastic_load_balancing/poll_healthy_instances.rb
@@ -4,14 +4,30 @@ module AwsHelpers
 
       class PollHealthyInstances
 
-        def initialize(config, load_balancer_name, required_instances, timeout)
+        IN_SERVICE = 'InService'
+        OUT_OF_SERVICE = 'OutOfService'
+        UNKNOWN = 'Unknown'
+
+        def initialize(stdout, config, load_balancer_name, expected_number, timeout)
+          @stdout = stdout
           @config = config
           @load_balancer_name = load_balancer_name
-          @required_instances = required_instances
+          @expected_number = expected_number
           @timeout = timeout
         end
 
         def execute
+          #TODO: Is expected_number actually needed?
+          client = @config.aws_elastic_load_balancing_client
+          client.wait_until(:instance_in_service, load_balancer_names: [@load_balancer_name]) { |waiter|
+            waiter.max_attempts = @timeout / waiter.delay
+            waiter.before_wait do |_attempts, response|
+              instance_states = response.instance_states
+              in_service_count = instance_states.select{|instance_state| instance_state.state == IN_SERVICE }.count
+              out_of_service = instance_states.count - in_service_count
+              @stdout.puts "In Service: #{in_service_count} Out of Service: #{out_of_service}"
+            end
+          }
 
         end
       end
