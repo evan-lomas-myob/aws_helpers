1b18b593957125b492db2631323286ac6dec5261
Paul Borg <paul.borg@myob.com>
2015-08-06 13:59:20 +1000

    Add class content and rspec for delete by time

--- lib/aws_helpers/actions/ec2/images_delete_by_time.rb
+++ lib/aws_helpers/actions/ec2/images_delete_by_time.rb
@@ -4,14 +4,19 @@ module AwsHelpers
 
       class ImagesDeleteByTime
 
-        def initialize(config, name, time)
+        def initialize(config, tag_name_value, creation_time)
           @config = config
-          @name = name
-          @time = time
+          @tag_name_value = tag_name_value
+          @creation_time = creation_time
         end
 
         def execute
-
+          response = ImagesFindByTags.new(@config, {name: 'Name', value: @tag_name_value}).execute
+          response.images.each do |image|
+            next unless image.state == 'available'
+            should_delete = Time.parse(image.creation_date) <= @creation_time
+            @config.aws_ec2_client.deregister_image(image_id: image.image_id) if should_delete
+          end
         end
 
       end
--- spec/lib/actions/ec2/images_delete_by_time_spec.rb
+++ spec/lib/actions/ec2/images_delete_by_time_spec.rb
@@ -1,11 +1,66 @@
 require 'aws_helpers/ec2'
 require 'aws_helpers/actions/ec2/images_delete_by_time'
 
-include AwsHelpers
 include AwsHelpers::Actions::EC2
 
 describe ImagesDeleteByTime do
 
+  describe '#execute' do
 
+    let(:aws_ec2_client) { instance_double(Aws::EC2::Client) }
+    let(:config) { instance_double(AwsHelpers::Config, aws_ec2_client: aws_ec2_client) }
+    let(:find_by_tags) { instance_double(ImagesFindByTags) }
+    let(:tag_name_value) { 'app_name' }
+    let(:creation_time) { Time.parse('1-Feb-2015 00:00:00') }
+
+    before(:each) {
+      allow(ImagesFindByTags).to receive(:new).and_return(find_by_tags)
+      allow(find_by_tags).to receive(:execute).and_return(images)
+      allow(aws_ec2_client).to receive(:deregister_image)
+    }
+
+    after(:each) {
+      ImagesDeleteByTime.new(config, tag_name_value, creation_time).execute
+    }
+
+    context 'with all images available' do
+
+      let(:images) {
+        double(images:
+                   [
+                       double(:first_image, image_id: 'first_image', creation_date: '1-Feb-2015 00:00:00', state: 'available'),
+                       double(:second_image, image_id: 'second_image', creation_date: '1-Feb-2015 00:00:01', state: 'available')
+                   ]) }
+
+      it 'should call ImagesFindByTags.new with correct parameters' do
+        expect(ImagesFindByTags).to receive(:new).with(config, {name: 'Name', value: tag_name_value}).and_return(find_by_tags)
+      end
+
+      it 'should call ImagesFindByTags execute method' do
+        expect(find_by_tags).to receive(:execute).and_return(images)
+      end
+
+      it 'should delete images older than creation_time' do
+        expect(aws_ec2_client).to receive(:deregister_image).with(image_id: 'first_image')
+      end
+
+    end
+
+    context 'with deregistered images' do
+
+      let(:images) {
+        double(images:
+                   [
+                       double(:first_image, image_id: 'first_image', creation_date: '1-Feb-2015 00:00:00', state: 'available'),
+                       double(:second_image, image_id: 'second_image', creation_date: '1-Feb-2015 00:00:00', state: 'something_else')
+                   ]) }
+
+      it 'should only delete available images' do
+        expect(aws_ec2_client).to receive(:deregister_image).with(image_id: 'first_image')
+      end
+
+    end
+
+  end
 
 end
