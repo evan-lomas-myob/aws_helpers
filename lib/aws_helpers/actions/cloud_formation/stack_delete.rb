911f611b7b02094dc0b7cde4f76cd22bf7e0b852
Paul Borg <paul.borg@myob.com>
2015-08-13 11:54:24 +1000

    Stack Delete Functionality and Testing

--- lib/aws_helpers/actions/cloud_formation/stack_delete.rb
+++ lib/aws_helpers/actions/cloud_formation/stack_delete.rb
@@ -4,14 +4,20 @@ module AwsHelpers
 
       class StackDelete
 
-        def initialize(config, stack_name)
+        def initialize(stdout, config, stack_name)
+          @stdout = stdout
+          @config = config
+          @stack_name = stack_name
         end
 
         def execute
+          @stdout.puts "Deleting #{@stack_name}"
+          client = @config.aws_cloud_formation_client
+          client.delete_stack(stack_name: @stack_name)
+          client.wait_until(:stack_delete_complete, stack_name: @stack_name)
         end
 
       end
-
     end
   end
 end
--- spec/unit/lib/actions/cloud_formation/stack_delete_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_delete_spec.rb
@@ -6,5 +6,17 @@ include AwsHelpers::Actions::CloudFormation
 
 describe StackDelete do
 
+  let(:cloudformation_client) { instance_double(Aws::CloudFormation::Client) }
+  let(:config) { instance_double(AwsHelpers::Config, aws_cloud_formation_client: cloudformation_client) }
+  let(:stdout) { instance_double(IO) }
+
+  let(:stack_name) { 'my_stack_name'}
+
+  it 'should call delete_stack to remove the stack' do
+    allow(cloudformation_client).to receive(:delete_stack).with(stack_name: stack_name)
+    allow(cloudformation_client).to receive(:wait_until).with(anything, stack_name: stack_name)
+    expect(stdout).to receive(:puts).with('Deleting my_stack_name')
+    AwsHelpers::Actions::CloudFormation::StackDelete.new(stdout, config, stack_name).execute
+  end
 
 end
