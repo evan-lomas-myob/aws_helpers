baa75cc9159cac099ce8e8ad44dbf792b0baad2c
Paul Borg <paul.borg@myob.com>
2015-08-13 16:57:50 +1000

    Create Error Events RSpec and refactor

--- spec/unit/lib/actions/cloud_formation/stack_error_events_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_error_events_spec.rb
@@ -4,44 +4,86 @@ require 'aws_helpers/actions/cloud_formation/stack_retrieve_events'
 require 'aws_helpers/actions/cloud_formation/stack_events_filter_post_initiation'
 require 'aws_helpers/actions/cloud_formation/stack_events_filter_failed'
 
+include AwsHelpers::Actions::CloudFormation
+
 describe StackErrorEvents do
 
   let(:cloudformation_client) { instance_double(Aws::CloudFormation::Client) }
   let(:config) { instance_double(AwsHelpers::Config, aws_cloud_formation_client: cloudformation_client) }
   let(:stdout) { instance_double(IO) }
 
-  let(:stack_retrieve_events) { instance_double(AwsHelpers::Actions::CloudFormation::StackRetrieveEvents)}
+  let(:stack_retrieve_events) { instance_double(AwsHelpers::Actions::CloudFormation::StackRetrieveEvents) }
+  let(:stack_events_filter_post_initiation) { instance_double(AwsHelpers::Actions::CloudFormation::StackEventsFilterPostInitiation) }
+  let(:stack_events_filter_failed) { instance_double(AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed) }
 
   let(:stack_name) { 'my_stack_name' }
+  let(:timestamp) { Time.parse('01-Jan-2015 00:00:00') }
   let(:next_token) { nil }
-  let(:resource_type) { 'AWS::CloudFormation::Stack' }
+  let(:logical_resource_id) { 'ResourceID' }
+  let(:resource_status_reason) { 'Success/failure message' }
 
   let(:initiation_event) { instance_double(Aws::CloudFormation::Event,
-                    stack_name: stack_name,
-                    resource_status: 'CREATE_IN_PROGRESS',
-                    resource_type: resource_type)
+                                           stack_name: stack_name,
+                                           timestamp: timestamp,
+                                           resource_status: 'CREATE_IN_PROGRESS',
+                                           logical_resource_id: logical_resource_id,
+                                           resource_status_reason: resource_status_reason)
   }
 
   let(:failed_event) { instance_double(Aws::CloudFormation::Event,
-                    stack_name: stack_name,
-                    resource_status: 'CREATE_FAILED',
-                    resource_type: resource_type)
+                                       stack_name: stack_name,
+                                       timestamp: timestamp,
+                                       resource_status: 'CREATE_FAILED',
+                                       logical_resource_id: logical_resource_id,
+                                       resource_status_reason: resource_status_reason)
   }
 
   let(:complete_event) { instance_double(Aws::CloudFormation::Event,
-                    stack_name: stack_name,
-                    resource_status: 'DELETE_COMPLETE',
-                    resource_type: resource_type )
+                                         stack_name: stack_name,
+                                         timestamp: timestamp,
+                                         resource_status: 'DELETE_COMPLETE',
+                                         logical_resource_id: logical_resource_id,
+                                         resource_status_reason: resource_status_reason)
   }
 
-  let(:stack_events) { [initiation_event, failed_event, complete_event] }
+  let(:stack_events) { [complete_event, failed_event, initiation_event] }
 
   let(:describe_stack_events_output) { instance_double(Aws::CloudFormation::Types::DescribeStackEventsOutput, stack_events: stack_events, next_token: next_token) }
 
-  it 'should call describe_stack_events to get events' do
-    allow(StackRetrieveEvents).to receive(:new).with(config, stack_name).and_return(stack_retrieve_events)
-    expect(stack_retrieve_events).to receive(:execute).and_return(stack_events)
+  before(:each) do
+    allow(cloudformation_client).to receive(:describe_stack_events).with(stack_name: stack_name, next_token: next_token).and_return(describe_stack_events_output)
+    allow(AwsHelpers::Actions::CloudFormation::StackRetrieveEvents).to receive(:new).with(config, stack_name).and_return(stack_retrieve_events)
+    allow(stack_retrieve_events).to receive(:execute).and_return(stack_events)
+    allow(AwsHelpers::Actions::CloudFormation::StackEventsFilterPostInitiation).to receive(:new).with(stack_events).and_return(stack_events_filter_post_initiation)
+    allow(stack_events_filter_post_initiation).to receive(:execute).and_return(stack_events)
+    allow(AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed).to receive(:new).with(stack_events).and_return(stack_events_filter_failed)
+    allow(stack_events_filter_failed).to receive(:execute).and_return(stack_events)
+    allow(stack_events_filter_failed).to receive(:execute).and_return(stack_events)
+    allow(stdout).to receive(:puts).with(anything)
+  end
+
+  after(:each) do
     AwsHelpers::Actions::CloudFormation::StackErrorEvents.new(stdout, config, stack_name).execute
   end
+
+  it 'should call StackRetrieveEvents to get events' do
+    expect(stack_retrieve_events).to receive(:execute).and_return(stack_events)
+  end
+
+  it 'should call StackEventsFilterPostInitiation to collect problem events' do
+    expect(stack_events_filter_post_initiation).to receive(:execute).and_return(stack_events)
+    AwsHelpers::Actions::CloudFormation::StackErrorEvents.new(stdout, config, stack_name).execute
+  end
+
+  it 'should call StackEventsFilterFailed filter out events that have failed' do
+    expect(stack_events_filter_failed).to receive(:execute).and_return(stack_events)
+    AwsHelpers::Actions::CloudFormation::StackErrorEvents.new(stdout, config, stack_name).execute
+  end
+
+  it 'should send event information to standard output' do
+    expect(stdout).to receive(:puts).with("#{timestamp}, DELETE_COMPLETE, #{logical_resource_id}, #{resource_status_reason}")
+    AwsHelpers::Actions::CloudFormation::StackErrorEvents.new(stdout, config, stack_name).execute
+  end
+
 
 end
