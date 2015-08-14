7ed8d06dd819207c90648cd4a8ee60ce0ae021cf
Paul Borg <paul.borg@myob.com>
2015-08-14 12:51:42 +1000

    Return true if the stack exists and false if it doesn't but rescue if the validation message is 'stack does not exist'

--- lib/aws_helpers/actions/cloud_formation/stack_exists.rb
+++ lib/aws_helpers/actions/cloud_formation/stack_exists.rb
@@ -12,10 +12,19 @@ module AwsHelpers
         end
 
         def execute
-
           client = @config.aws_cloud_formation_client
           stack = Aws::CloudFormation::Stack.new(@stack_name, client: client)
-          @stack_name if stack.stack_id
+
+          begin
+            true if stack.stack_id
+          rescue Aws::CloudFormation::Errors::ValidationError => validation_error
+            if validation_error.message == "Stack with id #{@stack_name} does not exist"
+              false
+            else
+              raise validation_error
+            end
+          end
+
         end
 
       end
