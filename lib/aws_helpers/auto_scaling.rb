94d1434de8f014b5dc80b63118a9a2c3b27a6dc3
Grant Tibbey <granttibbey@hotmail.com>
2015-08-15 15:18:44 +1000

    tweek Autoscaling #update_desired_capacity documentation

--- lib/aws_helpers/auto_scaling.rb
+++ lib/aws_helpers/auto_scaling.rb
@@ -24,25 +24,29 @@ module AwsHelpers
     # Changes the desired capacity of an auto scaling group.
     #
     # - Once changed will poll the group until the all servers reach an InService lifecycle status.
-    # - If load balancers are attached to the group, will also poll them until they also have a status of InService
+    # - If load balancers are attached to the group, will also poll them until they have a status of InService
     # @param auto_scaling_group_name [String] The group name of the Auto scaling client
     # @param desired_capacity [Integer] The capacity level of the auto scaling group
     # @param [Hash] options Optional parameters that can be overridden.
     # @option options [IO] :stdout Override $stdout when logging pooling output
     # @option options [Hash{Symbol => Integer}] :auto_scaling_polling Override auto scaling default polling
     #
-    #   ```
-    #   {
-    #     :max_attempts => 20,
-    #     :delay => 15
-    #   }
-    #   ```
-    # @option options [Hash{Symbol => Integer}] :load_balancer_polling Override load balancer default polling
+    #   defaults:
     #
     #   ```
     #   {
     #     :max_attempts => 20,
-    #     :delay => 15
+    #     :delay => 15 # seconds
+    #   }
+    #   ```
+    # @option options [Hash{Symbol => Integer}] :load_balancer_polling Override load balancer default polling
+    #
+    #   defaults:
+    #
+    #   ```
+    #   {
+    #     :max_attempts => 20,
+    #     :delay => 15 # seconds
     #   }
     #   ```
     def update_desired_capacity(auto_scaling_group_name, desired_capacity, options = {})
