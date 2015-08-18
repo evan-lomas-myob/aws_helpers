6807a2490631199d15b907d1b77247d14a493052
Paul Borg <paul.borg@myob.com>
2015-08-18 14:25:59 +1000

    S3 uplaod template RSpec

--- spec/unit/lib/actions/s3/upload_template_spec.rb
+++ spec/unit/lib/actions/s3/upload_template_spec.rb
@@ -12,6 +12,8 @@ describe S3UploadTemplate do
   let(:url) { 'https://my-bucket-url' }
   let(:stdout) { instance_double(IO) }
   let(:s3_template_url) { instance_double(S3TemplateUrl) }
+  let(:s3_exists) { instance_double(S3Exists) }
+  let(:s3_create) { instance_double(S3Create) }
 
   let(:stack_name) { 'my_stack_name' }
   let(:template_json) { 'json' }
@@ -21,32 +23,34 @@ describe S3UploadTemplate do
 
   let(:request) { {bucket: s3_bucket_name, key: stack_name, body: 'json', server_side_encryption: 'AES256'} }
 
-
-  it 'should check if the s3 bucket exists' do
-
+  before(:each) do
+    allow(aws_s3_client).to receive(:head_bucket)
+    allow(aws_s3_client).to receive(:put_object).with(request)
+    allow(S3TemplateUrl).to receive(:new).with(config, s3_bucket_name).and_return(s3_template_url)
+    allow(s3_template_url).to receive(:execute).and_return(url)
+    allow(stdout).to receive(:puts).with(anything)
   end
 
+  it 'should return a message that it will upload a template' do
+    expect(stdout).to receive(:puts).with("Uploading #{stack_name} to S3 bucket #{s3_bucket_name}")
+    S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute
+  end
 
-  # before(:each) do
-  #   allow(aws_s3_client).to receive(:put_object).with(request)
-  #   allow(aws_s3_client).to receive(:head_bucket)
-  #   allow(S3TemplateUrl).to receive(:new).with(config, s3_bucket_name).and_return(s3_template_url)
-  #   allow(s3_template_url).to receive(:execute).and_return(url)
-  #   allow(stdout).to receive(:puts).with(anything)
-  # end
+  it 'should create a new bucket if the bucket does not exist' do
+    allow(AwsHelpers::Actions::S3::S3Exists).to receive(:new).with(config, s3_bucket_name).and_return(s3_exists)
+    allow(s3_exists).to receive(:execute).and_return(false)
+    allow(AwsHelpers::Actions::S3::S3Create).to receive(:new).with(config, s3_bucket_name).and_return(s3_create)
+    expect(s3_create).to receive(:execute).and_return("Created S3 Bucket #{s3_bucket_name}")
+    S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute
+  end
 
-  # it 'should return a message that it will upload a template' do
-  #   expect(stdout).to receive(:puts).with("Uploading #{stack_name} to S3 bucket #{s3_bucket_name}")
-  #   S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute
-  # end
-  #
-  # it 'should generate a request and call put_object' do
-  #   expect(aws_s3_client).to receive(:put_object).with(request)
-  #   S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute
-  # end
-  #
-  # it 'should return the s3 bucket url' do
-  #   expect(S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute).to eq(url)
-  # end
+  it 'should call put object to load the template to the bucket' do
+    expect(aws_s3_client).to receive(:put_object).with(request)
+    S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute
+  end
+
+  it 'should return the s3 bucket url' do
+    expect(S3UploadTemplate.new(config, stack_name, template_json, s3_bucket_name, bucket_encrypt, stdout).execute).to eq(url)
+  end
 
 end
