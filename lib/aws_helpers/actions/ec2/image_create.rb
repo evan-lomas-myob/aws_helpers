400cc6c6c882b9d65ac44fe45ab841a53e35b126
Paul Borg <paul.borg@myob.com>
2015-08-20 15:35:38 +1000

    Refactor image_create to throw an exception if the instance is not in stopped or running state

--- lib/aws_helpers/actions/ec2/image_create.rb
+++ lib/aws_helpers/actions/ec2/image_create.rb
@@ -15,11 +15,17 @@ module AwsHelpers
 
         def execute
           client = @config.aws_ec2_client
+          check_image_state(client, @instance_id)
           image_id = client.create_image(instance_id: @instance_id, name: @instance_name)
           client.create_tags(resources: [image_id],
                              tags: [{key: 'Name', value: @instance_name}, {key: 'Date', value: @now.to_s}] + @additional_tags
           )
-          AwsHelpers::Actions::EC2::PollHealthyImages.new(@config, @instance_id, 1, 60).execute
+          end
+
+        def check_image_state(client, instance_id)
+          states = %w(running stopped)
+          image_status = client.describe_instance_status(instance_ids: [instance_id]).instance_statuses.first.instance_state.name
+          raise "AMI creation from #{instance_id} failed. State is #{image_status}" unless states.include?(image_status)
         end
 
       end
--- spec/unit/lib/actions/ec2/image_create_spec.rb
+++ spec/unit/lib/actions/ec2/image_create_spec.rb
@@ -1,7 +1,7 @@
 require 'time'
 require 'aws_helpers/ec2'
 require 'aws_helpers/actions/ec2/image_create'
-require 'aws_helpers/actions/ec2/poll_healthy_images'
+require 'aws_helpers/actions/ec2/poll_healthy_instances'
 
 include AwsHelpers
 include AwsHelpers::Actions::EC2
@@ -12,9 +12,7 @@ describe ImageCreate do
 
     let(:aws_ec2_client) { instance_double(Aws::EC2::Client) }
     let(:config) { instance_double(AwsHelpers::Config, aws_ec2_client: aws_ec2_client) }
-    let(:waiter) { instance_double(Aws::Waiters::Waiter, delay: 15, max_attempts: 40) }
 
-    let(:poll_healthy_images) { instance_double(PollHealthyImages) }
     let(:stdout) { instance_double(IO) }
 
     let(:image_id) { 'ami-abcd1234' }
@@ -24,6 +22,15 @@ describe ImageCreate do
 
     let(:additional_tag_name) { 'additional_tag_key' }
     let(:additional_tag_value) { 'additional_tag_value' }
+
+    let(:instance_state_good) { instance_double(Aws::EC2::Types::InstanceState, name: 'running') }
+    let(:instance_statuses_good) { [instance_double(Aws::EC2::Types::InstanceStatus, instance_state: instance_state_good)] }
+    let(:describe_instance_status_result_good) { instance_double(Aws::EC2::Types::DescribeInstanceStatusResult, instance_statuses: instance_statuses_good) }
+
+    let(:state_bad) { 'terminated'}
+    let(:instance_state_bad) { instance_double(Aws::EC2::Types::InstanceState, name: state_bad) }
+    let(:instance_statuses_bad) { [instance_double(Aws::EC2::Types::InstanceStatus, instance_state: instance_state_bad)] }
+    let(:describe_instance_status_result_bad) { instance_double(Aws::EC2::Types::DescribeInstanceStatusResult, instance_statuses: instance_statuses_bad) }
 
     let (:additional_tags) { [
         {key: 'Key', value: additional_tag_name},
@@ -35,26 +42,34 @@ describe ImageCreate do
     before(:each) do
       allow(aws_ec2_client).to receive(:create_image).with(instance_id: instance_id, name: instance_name).and_return(image_id)
       allow(aws_ec2_client).to receive(:create_tags).with(resources: [image_id], tags: [{key: 'Name', value: instance_name}, {key: 'Date', value: now.to_s}] + additional_tags)
-      allow(PollHealthyImages).to receive(:new).with(config, instance_id, 1, 60).and_return(poll_healthy_images)
-      allow(poll_healthy_images).to receive(:execute)
     end
 
-    after(:each) do
-      ImageCreate.new(config, instance_id, instance_name, additional_tags, now, stdout).execute
+    context 'instances is in a good state to get an image' do
+
+      after(:each) do
+        ImageCreate.new(config, instance_id, instance_name, additional_tags, now, stdout).execute
+      end
+
+      it 'should call the client create_image method with the correct arguments and return the image id' do
+        allow(aws_ec2_client).to receive(:describe_instance_status).with(instance_ids: [instance_id]).and_return(describe_instance_status_result_good)
+        expect(aws_ec2_client).to receive(:create_image).with(instance_id: instance_id, name: instance_name).and_return(image_id)
+      end
+
+      it 'should use the image id to add a default tag + additional tags to the new image' do
+        allow(aws_ec2_client).to receive(:describe_instance_status).with(instance_ids: [instance_id]).and_return(describe_instance_status_result_good)
+        expect(aws_ec2_client).to receive(:create_tags).with(resources: [image_id], tags: [{key: 'Name', value: instance_name}, {key: 'Date', value: now.to_s}] + additional_tags)
+      end
+
     end
 
-    it 'should call the client create_image method with the correct arguments and return the image id' do
-      expect(aws_ec2_client).to receive(:create_image).with(instance_id: instance_id, name: instance_name).and_return(image_id)
-    end
+    context 'instances is in a bad state to get an image' do
 
-    it 'should use the image id to add a default tag + additional tags to the new image' do
-      expect(aws_ec2_client).to receive(:create_tags).with(resources: [image_id], tags: [{key: 'Name', value: instance_name}, {key: 'Date', value: now.to_s}] + additional_tags)
-    end
+      it 'should raise an exception when the instance is not in a desirable state' do
+        allow(aws_ec2_client).to receive(:describe_instance_status).with(instance_ids: [instance_id]).and_return(describe_instance_status_result_bad)
+        expect { ImageCreate.new(config, instance_id, instance_name, additional_tags, now, stdout).execute }.to raise_error("AMI creation from #{instance_id} failed. State is #{state_bad}")
+      end
 
-    it 'should call PollHealthyImages until they are available' do
-      expect(poll_healthy_images).to receive(:execute)
     end
-
   end
 
 end
