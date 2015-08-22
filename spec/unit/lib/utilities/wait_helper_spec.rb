4dd8d7c723afd61d399740e9c5e4253c41fae135
Paul Borg <paul.borg@myob.com>
2015-08-22 14:46:41 +1000

    Add Coverage for Waiter

--- spec/unit/lib/utilities/wait_helper_spec.rb
+++ spec/unit/lib/utilities/wait_helper_spec.rb
@@ -8,16 +8,38 @@ describe WaitHelper do
 
   let(:aws_ec2_client) { instance_double(Aws::EC2::Client) }
   let(:config) { instance_double(AwsHelpers::Config, aws_ec2_client: aws_ec2_client) }
+
   let(:instance_id) { 'my_instance_id' }
 
   let(:timeout) { 60 }
   let(:waiter_names) { :instance_status_ok }
   let(:params) { double(:params, instance_id: [instance_id] ) }
 
+  let(:delay) { 1 }
+  let(:max_attempts) { 60 }
+
+  let(:waiter) { instance_double(Aws::Waiters::Waiter, delay: delay, max_attempts: max_attempts) }
+
   let(:block) { 'block' }
 
   it 'should call WaitHelper with the correct arguments' do
     expect(aws_ec2_client).to receive(:wait_until)
+    WaitHelper.wait(aws_ec2_client, timeout, waiter_names, params) { block }
+  end
+
+  it 'should receive max_attempts' do
+    allow(aws_ec2_client).to receive(:wait_until).and_yield(waiter)
+    allow(waiter).to receive(:before_wait)
+    allow(waiter).to receive(:delay).and_return(1.0)
+    expect(waiter).to receive(:max_attempts=)
+    WaitHelper.wait(aws_ec2_client, timeout, waiter_names, params) { block }
+  end
+
+  it 'should receive delay' do
+    allow(aws_ec2_client).to receive(:wait_until).and_yield(waiter)
+    allow(waiter).to receive(:before_wait)
+    expect(waiter).to receive(:delay).and_return(1.0)
+    allow(waiter).to receive(:max_attempts=)
     WaitHelper.wait(aws_ec2_client, timeout, waiter_names, params) { block }
   end
 
