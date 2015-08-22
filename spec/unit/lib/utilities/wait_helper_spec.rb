194d384dac384e91fdc9aa8a1d3ee302ca09e62a
Paul Borg <paul.borg@myob.com>
2015-08-22 17:54:48 +1000

    Try for 100% coverage

--- spec/unit/lib/utilities/wait_helper_spec.rb
+++ spec/unit/lib/utilities/wait_helper_spec.rb
@@ -13,34 +13,46 @@ describe WaitHelper do
 
   let(:timeout) { 60 }
   let(:waiter_names) { :instance_status_ok }
-  let(:params) { double(:params, instance_id: [instance_id] ) }
+  let(:params) { {} }
 
   let(:delay) { 1 }
   let(:max_attempts) { 60 }
 
+  let(:response) { instance_double(Seahorse::Client::Response) }
+
+  let(:block) { Proc.new { |_attempts, response| response } }
+
   let(:waiter) { instance_double(Aws::Waiters::Waiter, delay: delay, max_attempts: max_attempts) }
 
-  let(:block) { 'block' }
+  before(:each) do
+    allow(aws_ec2_client).to receive(:wait_until).with(waiter_names, params).and_yield(waiter)
+    allow(waiter).to receive(:before_wait)
+    allow(waiter).to receive(:delay).and_return(1.0)
+    allow(waiter).to receive(:max_attempts=)
+  end
 
-  it 'should call WaitHelper with the correct arguments' do
-    expect(aws_ec2_client).to receive(:wait_until)
+  after(:each) do
     WaitHelper.wait(aws_ec2_client, timeout, waiter_names, params) { block }
   end
 
-  it 'should receive max_attempts' do
-    allow(aws_ec2_client).to receive(:wait_until).and_yield(waiter)
-    allow(waiter).to receive(:before_wait)
-    allow(waiter).to receive(:delay).and_return(1.0)
-    expect(waiter).to receive(:max_attempts=)
-    WaitHelper.wait(aws_ec2_client, timeout, waiter_names, params) { block }
+  it 'should call WaitHelper with the correct arguments' do
+    expect(aws_ec2_client).to receive(:wait_until).with(waiter_names, params).and_yield(waiter)
+  end
+
+  it 'should call before_wait' do
+    expect(waiter).to receive(:before_wait)
   end
 
   it 'should receive delay' do
-    allow(aws_ec2_client).to receive(:wait_until).and_yield(waiter)
-    allow(waiter).to receive(:before_wait)
     expect(waiter).to receive(:delay).and_return(1.0)
-    allow(waiter).to receive(:max_attempts=)
-    WaitHelper.wait(aws_ec2_client, timeout, waiter_names, params) { block }
+  end
+
+  it 'should receive max_attempts' do
+    expect(waiter).to receive(:max_attempts=)
+  end
+
+  it 'should do something with a block' do
+    expect(block.call(1,response)).to eq(response)
   end
 
 end
