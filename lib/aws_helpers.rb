978fe9a565a24389a76aed1b07580966b54ecd0c
Paul Borg <paul.borg@myob.com>
2015-08-24 12:09:32 +1000

    Remove aws_helpers require of elastic_beanstalk utils

--- lib/aws_helpers.rb
+++ lib/aws_helpers.rb
@@ -2,7 +2,6 @@ require 'aws_helpers/version'
 require 'aws_helpers/auto_scaling'
 require 'aws_helpers/cloud_formation'
 require 'aws_helpers/ec2'
-require 'aws_helpers/elastic_beanstalk'
 require 'aws_helpers/elastic_load_balancing'
 require 'aws_helpers/rds'
 
