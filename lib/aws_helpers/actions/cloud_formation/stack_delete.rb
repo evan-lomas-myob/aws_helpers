ea6dccb5c85b35260917b84e97fa3fa59006dd84
Paul Borg <paul.borg@myob.com>
2015-11-05 08:38:21 +1100

    Update Stack Delete Action and Spec to Poll for Status

--- lib/aws_helpers/actions/cloud_formation/stack_delete.rb
+++ lib/aws_helpers/actions/cloud_formation/stack_delete.rb
@@ -7,14 +7,15 @@ module AwsHelpers
         def initialize(config, stack_name, options = {})
           @config = config
           @stack_name = stack_name
+          @options = options
           @stdout = options[:stdout] || $stdout
         end
 
         def execute
           client = @config.aws_cloud_formation_client
+          @stdout.puts "Deleting #{@stack_name}"
           client.delete_stack(stack_name: @stack_name)
-          client.wait_until(:stack_delete_complete, stack_name: @stack_name)
-          @stdout.puts "Deleted #{@stack_name}"
+          AwsHelpers::Actions::CloudFormation::PollStackStatus.new(@config, @stack_name, @options).execute
         end
 
       end
--- spec/unit/lib/actions/cloud_formation/stack_delete_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_delete_spec.rb
@@ -8,13 +8,16 @@ describe StackDelete do
 
   let(:cloudformation_client) { instance_double(Aws::CloudFormation::Client) }
   let(:config) { instance_double(AwsHelpers::Config, aws_cloud_formation_client: cloudformation_client) }
+  let(:poll_stack_delete) { instance_double(PollStackStatus) }
   let(:stdout) { instance_double(IO) }
+  let(:options) { {stdout: stdout} }
 
   let(:stack_name) { 'my_stack_name' }
 
   before(:each) do
     allow(cloudformation_client).to receive(:delete_stack)
-    allow(cloudformation_client).to receive(:wait_until)
+    allow(PollStackStatus).to receive(:new).with(config, stack_name, options).and_return(poll_stack_delete)
+    allow(poll_stack_delete).to receive(:execute)
     allow(stdout).to receive(:puts)
   end
 
@@ -26,12 +29,12 @@ describe StackDelete do
     expect(cloudformation_client).to receive(:delete_stack).with(stack_name: stack_name)
   end
 
-  it 'should wait until delete stack is completed' do
-    expect(cloudformation_client).to receive(:wait_until).with(anything, stack_name: stack_name)
+  it 'should poll for stack delete completion' do
+    expect(poll_stack_delete).to receive(:execute)
   end
 
   it 'should get output stating the stack was deleted' do
-    expect(stdout).to receive(:puts).with('Deleted my_stack_name')
+    expect(stdout).to receive(:puts).with('Deleting my_stack_name')
   end
 
 end
