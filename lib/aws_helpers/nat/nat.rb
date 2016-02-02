71aae7ed2bec81c1364d069b84b52e536fe6a529
andy shaw <shawry@shawry.com>
2016-02-03 10:08:04 +1100

    Add NAT helpers.

---
+++ lib/aws_helpers/nat/nat.rb
@@ -0,0 +1,33 @@
+require_relative 'client'
+
+module AwsHelpers
+
+  module NAT
+    class Gateway
+
+        # Utilities for creation and deletion of NAT gateways
+        def initialize(ec2_client)
+          @ec2_client = ec2_client
+        end
+
+        # Create a NAT gateway
+        # @param subnet_id [String] Id of the subnet to associate this gateway with
+        # @param allocation_id [String] ID of the allocation associated with an Elastic IP address
+        # @param [Hash] options Optional parameters that can be overridden.
+        # @return [String] the gateway id
+        def gateway_create(subnet_id, allocation_id)
+          result = @ec2_client.create_nat_gateway(subnet_id: subnet_id, allocation_id: allocation_id)
+          result.nat_gateway.nat_gateway_id
+        end
+
+        # Delete a NAT gateway
+        # @param gateway_id [String] the id of the NAT gateway
+        def gateway_delete(gateway_id)
+          @ec2_client.delete_nat_gateway(nat_gateway_id: gateway_id)
+        end
+    end
+  end
+
+end
+
+
