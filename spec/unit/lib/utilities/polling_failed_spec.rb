bc0bec607c245edf02675f6538b739e12f0627b3
Paul Borg <paul.borg@myob.com>
2016-03-17 16:14:51 +1100

    Add spec for polling failed

---
+++ spec/unit/lib/utilities/polling_failed_spec.rb
@@ -0,0 +1,63 @@
+require 'aws-sdk-core'
+require 'aws_helpers/utilities/polling_failed'
+
+#
+# module AwsHelpers
+#   module Utilities
+#
+#     class PollingFailed < StandardError; end
+#     class FailedStateError < PollingFailed; end
+#     class TooManyAttemptsError < PollingFailed
+#
+#       MSG = 'stopped waiting after %d attempts without success'
+#
+#       def initialize(attempts)
+#         @attempts = attempts
+#         super(MSG % [attempts])
+#       end
+#
+#       # @return [Integer]
+#       attr_reader :attempts
+#
+#     end
+#
+#   end
+# end
+#
+
+describe AwsHelpers::Utilities::PollingFailed do
+
+  it 'should return and instance of StandardError' do
+    expect(AwsHelpers::Utilities::PollingFailed.new).to be_a_kind_of(StandardError)
+  end
+
+end
+
+describe AwsHelpers::Utilities::FailedStateError do
+
+  it 'should return and instance of StandardError' do
+    expect(AwsHelpers::Utilities::FailedStateError.new).to be_a_kind_of(StandardError)
+  end
+
+end
+
+describe AwsHelpers::Utilities::FailedStateError do
+
+  let(:attempts) { 5 }
+  let(:response) { instance_double(AwsHelpers::Utilities::TooManyAttemptsError, "stopped waiting after #{attempts} attempts without success") }
+
+  it 'should return and instance of PollingFailed' do
+    expect(AwsHelpers::Utilities::TooManyAttemptsError.new(attempts)).to be_a_kind_of(AwsHelpers::Utilities::PollingFailed)
+  end
+
+  it 'should return a PollingFailed with message' do
+    allow(AwsHelpers::Utilities::PollingFailed).to receive(:new).with(5)
+    expect(AwsHelpers::Utilities::TooManyAttemptsError.new(attempts)).to be(response)
+  end
+
+  it 'should return the number of attempts' do
+    expect(AwsHelpers::Utilities::TooManyAttemptsError.new(attempts).attempts).to eq(5)
+  end
+
+end
+
