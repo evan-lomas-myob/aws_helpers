fa505986e62d4ab9087daac9a974082be25fd560
Paul Borg <paul.borg@myob.com>
2016-03-17 16:15:05 +1100

    Remove comments

--- spec/unit/lib/utilities/polling_failed_spec.rb
+++ spec/unit/lib/utilities/polling_failed_spec.rb
@@ -1,30 +1,6 @@
 require 'aws-sdk-core'
 require 'aws_helpers/utilities/polling_failed'
 
-#
-# module AwsHelpers
-#   module Utilities
-#
-#     class PollingFailed < StandardError; end
-#     class FailedStateError < PollingFailed; end
-#     class TooManyAttemptsError < PollingFailed
-#
-#       MSG = 'stopped waiting after %d attempts without success'
-#
-#       def initialize(attempts)
-#         @attempts = attempts
-#         super(MSG % [attempts])
-#       end
-#
-#       # @return [Integer]
-#       attr_reader :attempts
-#
-#     end
-#
-#   end
-# end
-#
-
 describe AwsHelpers::Utilities::PollingFailed do
 
   it 'should return and instance of StandardError' do
