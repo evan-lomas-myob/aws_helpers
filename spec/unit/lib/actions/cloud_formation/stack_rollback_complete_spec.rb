99d5952e1194531156de652217e44a56ed48316d
Grant Tibbey <granttibbey@hotmail.com>
2016-03-23 21:31:40 +1100

    refactor CloudFormation stack rollback complete

--- spec/unit/lib/actions/cloud_formation/stack_rollback_complete_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_rollback_complete_spec.rb
@@ -2,35 +2,32 @@ require 'aws-sdk-core'
 require 'aws_helpers/config'
 require 'aws_helpers/actions/cloud_formation/stack_rollback_complete'
 
-include Aws::CloudFormation::Types
-include AwsHelpers::Actions::CloudFormation
-
-describe StackRollbackComplete do
+describe AwsHelpers::Actions::CloudFormation::StackRollbackComplete do
 
   let(:cloudformation_client) { instance_double(Aws::CloudFormation::Client) }
   let(:config) { instance_double(AwsHelpers::Config, aws_cloud_formation_client: cloudformation_client) }
 
   let(:stack_name) { 'my_stack_name' }
+  let(:rollback_complete) { create_response('ROLLBACK_COMPLETE') }
+  let(:create_complete) { create_response('CREATE_COMPLETE') }
 
-  let(:stack_rollback_complete) { [
-      instance_double(Aws::CloudFormation::Stack, stack_name: stack_name, stack_status: 'ROLLBACK_COMPLETE')
-  ] }
+  subject { AwsHelpers::Actions::CloudFormation::StackRollbackComplete.new(config, stack_name).execute }
 
-  let(:stack_create_complete) { [
-      instance_double(Aws::CloudFormation::Stack, stack_name: stack_name, stack_status: 'CREATE_COMPLETE')
-  ] }
-
-  let(:stack_rolledback) { instance_double(DescribeStacksOutput, stacks: stack_rollback_complete) }
-  let(:stack_ok_response) { instance_double(DescribeStacksOutput, stacks: stack_create_complete) }
-
-  it 'should return false if stack is in status ROLLBACK_COMPLETE' do
-    allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(stack_rolledback)
-    expect(StackRollbackComplete.new(config, stack_name).execute).to eq(true)
+  it 'should return true if stack is in status ROLLBACK_COMPLETE' do
+    allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(rollback_complete)
+    expect(subject).to eq(true)
   end
 
-  it 'should return false if stack is not status ROLLBACK_COMPLETE' do
-    allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(stack_ok_response)
-    expect(StackRollbackComplete.new(config, stack_name).execute).to eq(false)
+  it 'should return false if stack is any other status' do
+    allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(create_complete)
+    expect(subject).to eq(false)
   end
+
+  def create_response(status)
+    Aws::CloudFormation::Types::DescribeStacksOutput.new(stacks:[
+      Aws::CloudFormation::Types::Stack.new(stack_status: status)
+    ])
+  end
+
 
 end
