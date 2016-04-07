2689eb9a625d9d5587fa7908f7885d43af9b2044
Grant Tibbey <granttibbey@hotmail.com>
2016-04-07 15:05:53 +1000

    refactor specs

--- spec/unit/lib/actions/cloud_formation/stack_create_request_builder_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_create_request_builder_spec.rb
@@ -1,47 +1,36 @@
-require 'aws-sdk-core'
-require 'aws_helpers/cloud_formation'
 require 'aws_helpers/actions/cloud_formation/stack_create_request_builder'
 
-include Aws::CloudFormation::Types
-include AwsHelpers
-include AwsHelpers::Actions::CloudFormation
-
-describe StackCreateRequestBuilder do
+describe AwsHelpers::Actions::CloudFormation::StackCreateRequestBuilder do
   let(:stack_name) { 'my_stack_name' }
-  let(:s3_template_url) { instance_double(S3TemplateUrl) }
+  let(:s3_template_url) { 'url' }
   let(:template_json) { 'json' }
-  let(:parameters) do
-    [
-      Parameter.new(parameter_key: 'param_key_1', parameter_value: 'param_value_1'),
-      Parameter.new(parameter_key: 'param_key_2', parameter_value: 'param_value_1')
-    ]
-  end
-
+  let(:parameters) { [{parameter_key: 'key', parameter_value: 'value'}] }
   let(:capabilities) { ['CAPABILITY_IAM'] }
 
   let(:request_with_url) do
     {
-      stack_name: stack_name,
-      template_url: s3_template_url,
-      parameters: parameters,
-      capabilities: capabilities
+        stack_name: stack_name,
+        template_url: s3_template_url,
+        parameters: parameters,
+        capabilities: capabilities
     }
   end
 
   let(:request_without_url) do
     {
-      stack_name: stack_name,
-      template_body: template_json,
-      parameters: parameters,
-      capabilities: capabilities
+        stack_name: stack_name,
+        template_body: template_json,
+        parameters: parameters,
+        capabilities: capabilities
     }
   end
 
   it 'should create the request hash with the s3 bucket url' do
-    expect(StackCreateRequestBuilder.new(stack_name, s3_template_url, template_json, parameters, capabilities).execute).to eq(request_with_url)
+    expect(described_class.new(stack_name, s3_template_url, template_json, parameters, capabilities).execute).to eq(request_with_url)
   end
 
   it 'should create the request hash with the json template' do
-    expect(StackCreateRequestBuilder.new(stack_name, nil, template_json, parameters, capabilities).execute).to eq(request_without_url)
+    expect(described_class.new(stack_name, nil, template_json, parameters, capabilities).execute).to eq(request_without_url)
   end
+
 end
