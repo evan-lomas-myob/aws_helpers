9b2e1d2fbd7fc9116fe6e98b3ee744d849374a0d
Grant Tibbey <granttibbey@hotmail.com>
2016-04-08 14:19:32 +1000

    refactor and stack events filter failed spec

--- spec/unit/lib/actions/cloud_formation/stack_events_filter_failed_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_events_filter_failed_spec.rb
@@ -1,32 +1,32 @@
-require 'aws_helpers/actions/cloud_formation/stack_events_filter_failed'
+require 'aws_helpers'
 require_relative '../../../spec_helpers/create_event_helper'
 
-include AwsHelpers::Actions::CloudFormation
-
-describe StackEventsFilterFailed do
-  let(:stack_id) { 'id' }
-  let(:stack_name) { 'my_stack_name' }
-  let(:resource_type) { 'AWS::CloudFormation::Stack' }
-  let(:resource_type_bad) { 'AWS::EC2::Instance' }
-
-  let(:create_failed_event) { CreateEventHelper.new(stack_name, stack_id, 'CREATE_FAILED', resource_type).execute }
-  let(:delete_failed_event) { CreateEventHelper.new(stack_name, stack_id, 'DELETE_FAILED', resource_type).execute }
-  let(:update_failed_event) { CreateEventHelper.new(stack_name, stack_id, 'UPDATE_FAILED', resource_type).execute }
-  let(:rollback_failed_event) { CreateEventHelper.new(stack_name, stack_id, 'ROLLBACK_FAILED', resource_type).execute }
-  let(:update_rollback_failed_event) { CreateEventHelper.new(stack_name, stack_id, 'UPDATE_ROLLBACK_FAILED', resource_type).execute }
-  let(:create_failed_event_wrong_type) { CreateEventHelper.new(stack_name, stack_id, 'CREATE_FAILED', resource_type_bad).execute }
-  let(:wrong_event) { CreateEventHelper.new(stack_name, stack_id, 'DELETE_COMPLETE', resource_type).execute }
-
-  let(:stack_events_good) { [create_failed_event, delete_failed_event, update_failed_event, rollback_failed_event, update_rollback_failed_event] }
-  let(:stack_events_bad_resource) { [create_failed_event, delete_failed_event, wrong_event, update_failed_event, rollback_failed_event, update_rollback_failed_event] }
+describe AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed do
 
   describe '#execute' do
-    it 'should return the whole array stack because the events have matching resource status' do
-      expect(AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed.new(stack_events_good).execute).to eq(stack_events_good)
+
+    context 'failed events' do
+
+      %w(CREATE_FAILED DELETE_FAILED UPDATE_FAILED ROLLBACK_FAILED UPDATE_ROLLBACK_FAILED).each { |status|
+        events = [CreateEventHelper.new('name', 'id', status, 'AWS::CloudFormation::Stack').execute]
+        it "should return the #{status} event" do
+          expect(AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed.new(events).execute).to eql(events)
+        end
+      }
+
     end
 
-    it 'should drop the event from array stack because the event has the wrong resource status' do
-      expect(AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed.new(stack_events_bad_resource).execute).to eq(stack_events_good)
+    context 'other events' do
+
+      %w(OTHER).each { |status|
+        events = [CreateEventHelper.new('name', 'id', status, 'AWS::CloudFormation::Stack').execute]
+        it 'should return an empty array' do
+          expect(AwsHelpers::Actions::CloudFormation::StackEventsFilterFailed.new(events).execute).to eql([])
+        end
+      }
+
     end
+
+
   end
 end
