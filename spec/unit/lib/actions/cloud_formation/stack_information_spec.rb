ce784849c4f3f4f9151f763f67ea5cd560d60b5d
Grant Tibbey <granttibbey@hotmail.com>
2016-04-08 14:40:12 +1000

    refactor stack information spec

--- spec/unit/lib/actions/cloud_formation/stack_information_spec.rb
+++ spec/unit/lib/actions/cloud_formation/stack_information_spec.rb
@@ -1,47 +1,33 @@
-require 'aws_helpers/cloud_formation'
-require 'aws_helpers/actions/cloud_formation/stack_information'
+require 'aws_helpers'
 
-include AwsHelpers
-include AwsHelpers::Actions::CloudFormation
-include Aws::CloudFormation::Types
-
-describe StackInformation do
+describe AwsHelpers::Actions::CloudFormation::StackInformation do
   let(:cloudformation_client) { instance_double(Aws::CloudFormation::Client) }
   let(:config) { instance_double(AwsHelpers::Config, aws_cloud_formation_client: cloudformation_client) }
 
   let(:stack_name) { 'my_stack_name' }
+  let(:parameters) { [Aws::CloudFormation::Types::Parameter.new] }
+  let(:outputs) { [Aws::CloudFormation::Types::Output.new] }
+  let(:response) {
+    Aws::CloudFormation::Types::DescribeStacksOutput.new(
+        stacks: [
+            Aws::CloudFormation::Types::Stack.new(stack_name: stack_name, parameters: parameters, outputs: outputs)
+        ])
+  }
 
-  let(:parameters) do
-    [
-      instance_double(Parameter, parameter_key: 'param_key_1', parameter_value: 'param_value_1'),
-      instance_double(Parameter, parameter_key: 'param_key_1', parameter_value: 'param_value_1')
-    ]
+  describe '#execute' do
+
+    before(:each) do
+      allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(response)
+    end
+
+    it 'should return stack parameters' do
+      expect(described_class.new(config, stack_name, 'parameters').execute).to eq(parameters)
+    end
+
+    it 'should return stack outputs' do
+      expect(described_class.new(config, stack_name, 'outputs').execute).to eq(outputs)
+    end
+
   end
 
-  let(:outputs) do
-    [
-      instance_double(Output, output_key: 'output_key_1', output_value: 'output_value_1', description: 'output_description_1'),
-      instance_double(Output, output_key: 'output_key_2', output_value: 'output_value_2', description: 'output_description_2')
-    ]
-  end
-
-  let(:stack_list) do
-    [
-      instance_double(Stack, stack_name: stack_name, parameters: parameters, outputs: outputs)
-    ]
-  end
-
-  let(:response) { instance_double(DescribeStacksOutput, stacks: stack_list) }
-
-  it 'should return stack parameters' do
-    info_field = 'parameters'
-    allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(response)
-    expect(StackInformation.new(config, stack_name, info_field).execute).to eq(parameters)
-  end
-
-  it 'should return stack outputs' do
-    info_field = 'outputs'
-    allow(cloudformation_client).to receive(:describe_stacks).with(stack_name: stack_name).and_return(response)
-    expect(StackInformation.new(config, stack_name, info_field).execute).to eq(outputs)
-  end
 end
