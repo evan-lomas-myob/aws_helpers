8d157a881ca7b649c6c6a2b6ea7d2fb60bb91f8d
andy shaw <shawry@shawry.com>
2016-05-18 10:10:02 +1000

    Fix bug in polling db snapshots - permissions.

--- lib/aws_helpers/rds_commands/commands/poll_snapshot_available_command.rb
+++ lib/aws_helpers/rds_commands/commands/poll_snapshot_available_command.rb
@@ -9,18 +9,19 @@ module AwsHelpers
 
         def initialize(config, request)
           @rds_client = config.aws_rds_client
-          @snapshot_name = request.snapshot_name
+          # @request.snapshot_name = request.snapshot_name
           @request = request
         end
 
         def execute
           poll(@request.snapshot_polling[:delay], @request.snapshot_polling[:max_attempts]) do
-            response = @rds_client.describe_db_snapshots(db_snapshot_identifier: @snapshot_name)
-            snapshot = response.db_snapshots.find { |s| s.db_snapshot_identifier == @snapshot_name }
+            response = @rds_client.describe_db_snapshots(db_instance_identifier: @request.db_instance_id)
+            puts reponse.db_snapshots
+            snapshot = response.db_snapshots.find { |s| s.db_snapshot_identifier == @request.snapshot_name }
             status = snapshot.status
             percent_progress = snapshot.percent_progress
-            std_out.puts "RDS Snapshot #{@snapshot_name} #{status}, progress #{percent_progress}%"
-            raise "RDS Failed to create snapshot #{@snapshot_name}" if status == 'deleting'
+            std_out.puts "RDS Snapshot #{@request.snapshot_name} #{status}, progress #{percent_progress}%"
+            raise "RDS Failed to create snapshot #{@request.snapshot_name}" if status == 'deleting'
             status == 'available'
           end
         end
