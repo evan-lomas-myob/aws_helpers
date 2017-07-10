593560f15aa9679fc9c1f73c0a8610944fbdd341
Evan Lomas <evan.lomas@myob.com>
2017-07-10 16:31:46 +1000

    further redshift setup

--- lib/aws_helpers/actions/redshift/cluster_create.rb
+++ lib/aws_helpers/actions/redshift/cluster_create.rb
@@ -20,6 +20,7 @@ module AwsHelpers
           @master_user_password = master_user_password
           @db_name = options[:db_name] ||= $db_name
           @node_type = options[:node_type] ||= $node_type
+          @vpc_id = options[:vpc_id] ||= $vpc_id
           @enhanced_vpc_routing = options[:enhanced_vpc_routing] ||= $enhanced_vpc_routing
           @vpc_security_group_ids = options[:vpc_security_group_ids] ||= $vpc_security_group_ids
         end
@@ -33,6 +34,7 @@ module AwsHelpers
             master_username: @master_username,
             master_user_password: @master_user_password,
             node_type: @node_type,
+            vpc_id: @vpc_id,
             enhanced_vpc_routing: @enhanced_vpc_routing,
             vpc_security_group_ids: @vpc_security_group_ids
           )
