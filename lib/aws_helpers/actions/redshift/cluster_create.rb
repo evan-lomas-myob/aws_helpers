9ae2f50f98c65f9429349d2fd2e037ba174e71da
Evan Lomas <evan.lomas@myob.com>
2017-07-11 08:54:44 +1000

    Removed vpc params for redshift (not working)

--- lib/aws_helpers/actions/redshift/cluster_create.rb
+++ lib/aws_helpers/actions/redshift/cluster_create.rb
@@ -10,8 +10,6 @@ module AwsHelpers
         include AwsHelpers::Utilities::PollingOptions
 
         def initialize(config, cluster_type, cluster_identifier, master_username, master_user_password, options)
-          puts 'options:'
-          puts options
           @config = config
           @client = config.aws_redshift_client
           @cluster_type = cluster_type
@@ -20,8 +18,8 @@ module AwsHelpers
           @master_user_password = master_user_password
           @db_name = options[:db_name] ||= $db_name
           @node_type = options[:node_type] ||= $node_type
-          @enhanced_vpc_routing = options[:enhanced_vpc_routing] ||= $enhanced_vpc_routing
-          @vpc_security_group_ids = options[:vpc_security_group_ids] ||= $vpc_security_group_ids
+          # @enhanced_vpc_routing = options[:enhanced_vpc_routing] ||= $enhanced_vpc_routing
+          # @vpc_security_group_ids = options[:vpc_security_group_ids] ||= $vpc_security_group_ids
         end
 
         def execute
@@ -32,9 +30,9 @@ module AwsHelpers
             db_name: @db_name,
             master_username: @master_username,
             master_user_password: @master_user_password,
-            node_type: @node_type,
-            enhanced_vpc_routing: @enhanced_vpc_routing,
-            vpc_security_group_ids: @vpc_security_group_ids
+            node_type: @node_type
+            # enhanced_vpc_routing: @enhanced_vpc_routing,
+            # vpc_security_group_ids: @vpc_security_group_ids
           )
 
           AwsHelpers::Actions::Redshift::PollInstanceExists.new(@config, instance_id, @instance_exists_polling).execute
